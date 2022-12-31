38bab819ef3e6f520efa0eac5a5ba0fe4b54152e
Kennynyamai <kennynyamai73@gmail.com>
2022-12-30 21:55:46 -0800

    folder for functions

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"main.h"
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+	int sum = add(a, b);
+	printf("sum is %d",sum);
+	return 0;
+}
---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int add(int a, int b);
+void _abs(void);
+void _isalpha(void);
+void _islower(void);
+void _putchar(void);
+
+#endif
